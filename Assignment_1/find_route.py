27cd6c5b7a776ef9de37d110e457b44496d6a5eb
Yashodhan Kumthekar <ykumthekar4929@gmail.com>
2018-01-26 18:52:28 -0600

    Assignment 1 completed

--- Assignment_1/find_route.py
+++ Assignment_1/find_route.py
@@ -1,65 +1,97 @@
-import pprint
-from operator import itemgetter
 # config
-filepath = ('input1.txt')
 end_of_file = 'END OF INPUT'
-end_of_file_copy = 'END OF INPUT'
-pp = pprint.PrettyPrinter(indent=2)
-# declarations
-input_file = open(filepath)
-all_the_paths = []
-sample_graph = {}
+
+# initialisations
+dataset_raw = []
+dataset_tree = {}
+initial_city =''
+goal_city = ''
 cities_corpus = []
-
-class path_object(object):
-    def __init__(self):
-        self.city_one = None
-        self.city_two = None
-        self.distance = None
-    def __unicode__(self):
-        return self.city_one
-
+fringe = []
 
 def format_to_graph():
+    global cities_corpus
+    filepath = raw_input('Give path of the input file: ')
+    input_file = open(filepath)
     for cnt, line in enumerate(input_file):
         if line.strip() != end_of_file.strip():
-            all_the_paths.append(line.strip().split(" "))
-    for each in all_the_paths:
+            dataset_raw.append(line.strip().split(" "))
+    for each in dataset_raw:
         cities_corpus.append(each[0].strip())
         cities_corpus.append(each[1].strip())
     cities_corpus =  list(set(cities_corpus))
     for city in cities_corpus:
         paths_from_city = []
-        for line in all_the_paths:
+        for line in dataset_raw:
             if line[0] == city:
                 paths_from_city.append({'destination':line[1], 'distance':int(line[2])})
             elif line [1] == city:
                 paths_from_city.append({'destination':line[0], 'distance':int(line[2])})
             else:
                 pass
-        sample_graph.update({city:paths_from_city})
+        dataset_tree.update({city:paths_from_city})
 
 
-def check_for_goal_city(node_to_check_from):
-    for each in current_city_nodes:
-        print initial_city, each
-
-def driver(initial_city, goal_city):
-
-    if initial_city != goal_city and initial_city in cities_corpus and goal_city in cities_corpus:
-        current_city_nodes = sample_graph.get(initial_city)
-        current_city_nodes = sorted(current_city_nodes, key=itemgetter('distance'), reverse=False)
+def uc_search(current_city):
+    global fringe
+    while fringe:
+        if goal_city == fringe[0][0][-1]:
+            break
+        else:
+            dequed_element = fringe[0]
+            del fringe[0]
+            dequed_element_children = dataset_tree[dequed_element[0][-1]]
+            for item in dequed_element_children:
+                if set((item['destination'],)).issubset(dequed_element[0]):
+                    pass
+                else:
+                    record_to_append = [(dequed_element[0] + (item['destination'],)), dequed_element[1] + item['distance']]
+                    fringe.append(record_to_append)
+            fringe = sorted(fringe, key = lambda x: (int(x[1]), x[0][-1]))
     else:
-        print "No route for these cities"
+        fringe = None
+    return fringe
 
 
+def print_input_error(initial_city, goal_city):
+    if initial_city == goal_city:
+        print "Cities are same"
+    elif initial_city not in cities_corpus or goal_city not in cities_corpus:
+        print "Either or both of the cities not in dataset"
+    show_prompt()
 
+
+def driver(initial_city):
+    global fringe
+    if initial_city != goal_city and initial_city in cities_corpus and goal_city in cities_corpus:
+        fringe.append([(initial_city,), 0])
+        search_result = uc_search(initial_city)
+        if search_result is None:
+            print "Distance: infinity \nRoute: \nNone"
+        else:
+            search_result = search_result[0]
+            print "Distance: %s"%search_result[1]
+            route_points = search_result[0]
+            print "Route: "
+            for x in range(0,len(route_points) - 1):
+                print "%s  %s  %s"%(route_points[x], route_points[x + 1], [item for item in dataset_tree[route_points[x]] if item['destination'] == route_points[x+1]][0]['distance'])
+        show_prompt()
+    else:
+        print_input_error(initial_city, goal_city)
+
+
+def show_prompt():
+    global goal_city
+    global fringe
+    initial_city = raw_input("Initial city : ")
+    goal_city = raw_input("Goal city : ")
+    fringe = []
+    driver(initial_city)
 
 
 def main():
     format_to_graph()
-    initial_city = raw_input("What is your initial_city? ")
-    goal_city = raw_input("What is your goal_city? ")
-    driver('Luebeck', 'Bremen')
+    show_prompt()
 
-main()
+if __name__ == "__main__":
+    main()
